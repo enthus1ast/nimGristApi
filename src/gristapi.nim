1b7d52059dd9d8b4df342d5821b2899e58f4d3f2
David Krause <enthus1ast@users.noreply.github.com>
2023-01-17 17:30:55 +0100

    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- src/gristapi.nim
+++ src/gristapi.nim
@@ -1,11 +1,17 @@
-import asyncdispatch, httpclient, strutils, uri, tables, json, strformat
+# import asyncdispatch, httpclient,
+import strutils, uri, tables, json, strformat
 import std/jsonutils
+import puppy
+# import httpcore
 
 type
-  GristApi = object
-    server: Uri
-    docId: string
-    client: AsyncHttpClient
+  # GristApi[AsyncHttpClient | * = object
+  GristApi* = object
+    server*: Uri
+    docId*: string
+    # client*: AsyncHttpClient
+    # headers*: HttpHeaders
+    headers*: seq[(string, string)]
   # GristValueKind = enum
   #   gvString
   #   gvNumber
@@ -18,59 +24,60 @@ type
   Rows = seq[Row]
 
 
-proc newGristApi(docId, apiKey: string, server: Uri | string): GristApi =
+proc newGristApi*(docId, apiKey: string, server: Uri | string): GristApi =
   result.docId = docId
   when server is Uri:
     result.server = server
   else:
     result.server = parseUri(server)
-  result.client = newAsyncHttpClient()
-  result.client.headers = newHttpHeaders(
-    {"Authorization": fmt"Bearer {apiKey}"}
-  )
+  # result.client = newAsyncHttpClient()
+  # result.client.headers = newHttpHeaders(
+  result.headers = @[("Authorization", fmt"Bearer {apiKey}")]
 
-proc addRecords(grist: GristApi, tableId: string, data: Rows) =
-  discard
+proc get(grist: GristApi, url: Uri, headers: seq[(string, string)] = @[]): string =
+  var combinedHeaders = grist.headers & headers
+  return get($url, combinedHeaders).body
 
-proc fetchTable(grist: GristApi, table: string, filters: Table[string, string]): Future[Rows] {.async.} =
-  let path = fmt"/api/docs/{grist.docId}/tables/{table}/records"
-  var url = grist.server / path
-  echo "#############################"
-  # echo encodeQuery(%* filters)
-  # echo $ (%* filters)
-  echo "#############################"
 
-  # if filters.len > 0:
-    # url = url ? filters
+# proc addRecords*(grist: GristApi, tableId: string, data: Rows) =
+#   discard
 
-  let body = await (await grist.client.get(url)).body
-  let js = parseJson(body)
-  var ret: seq[JsonNode] = @[]
-  if js.hasKey("records"):
-    for record in js["records"].getElems():
-      ret.add record
-    return ret
+# proc fetchTable*(grist: GristApi, table: string, filters: Table[string, string]): Rows  =
+#   let path = fmt"/api/docs/{grist.docId}/tables/{table}/records"
+#   var url = grist.server / path
+#   echo "#############################"
+#   # echo encodeQuery(%* filters)
+#   # echo $ (%* filters)
+#   echo "#############################"
 
-proc downloadXLSX(grist: GristApi): Future[string] {.async.} =
+#   # if filters.len > 0:
+#     # url = url ? filters
+
+#   let body = await (await grist.client.get(url)).body
+#   let js = parseJson(body)
+#   var ret: seq[JsonNode] = @[]
+#   if js.hasKey("records"):
+#     for record in js["records"].getElems():
+#       ret.add record
+#     return ret
+
+proc downloadXLSX*(grist: GristApi): string  =
   ## Download and returns the document as XLSX
   let path = fmt"/api/docs/{grist.docId}/download/xlsx"
   var url = grist.server / path
-  let resp =  (await grist.client.get(url))
-  return (await resp.body)
+  return grist.get(url)
 
-proc downloadCSV(grist: GristApi, tableId: string): Future[string] {.async.} =
+proc downloadCSV*(grist: GristApi, tableId: string): string  =
   ## Download and returns the document as CSV
   let path = fmt"/api/docs/{grist.docId}/download/csv?tableId={tableId}"
   var url = grist.server / path
-  let resp =  (await grist.client.get(url))
-  return (await resp.body)
+  return grist.get(url)
 
-proc downloadSQLITE(grist: GristApi): Future[string] {.async.} =
+proc downloadSQLITE*(grist: GristApi): string  =
   ## Download and returns the document as SQLITE
   let path = fmt"/api/docs/{grist.docId}/download"
   var url = grist.server / path
-  let resp =  (await grist.client.get(url))
-  return (await resp.body)
+  return grist.get(url)
 
 
 when isMainModule and true:
@@ -79,40 +86,42 @@ when isMainModule and true:
     docId = "kTveggjMFamxzQL7AbFoxu",
     apiKey = "d17a2270af40484d3592cba694157b9fb720e01a",
     server = "http://192.168.174.238:8484/"
+    # server = "http://172.16.3.210:8484/"
+
   )
 
   let dateStr = ($now()).replace(":", "-")
 
-  let dataSqlite = waitFor grist.downloadSQLITE()
+  let dataSqlite = grist.downloadSQLITE()
   writeFile(fmt"gene__{dateStr}.sqlite", dataSqlite)
 
-  let dataXLSX = waitFor grist.downloadXLSX()
+  let dataXLSX = grist.downloadXLSX()
   writeFile(fmt"gene__{dateStr}.xlsx", dataXLSX)
 
-  let dataCSV = waitFor grist.downloadCSV("Entries")
+  let dataCSV = grist.downloadCSV("Entries")
   writeFile(fmt"geneEntries__{dateStr}.csv", dataCSV)
 
-when isMainModule and false:
-  var grist = newGristApi(
-    docId = "v2GoPdCvmv5p",
-    apiKey = "da073fa6d424d4126e65a8a054a6693de19f485e",
-    server = "http://127.0.0.1:8484/"
-  )
-  # var emails: OrderedTable[int, JsonNode]
-  # for elem in waitFor grist.fetchTable("Emails"):
-  #   echo elem
-  #   emails[elem["id"].getInt] = elem
-  #   # echo elem{"fields", "User"}.getStr(), " " ,  elem{"fields", "Email"}.getStr()
-  #   # echo elem
+# when isMainModule and false:
+#   var grist = newGristApi(
+#     docId = "v2GoPdCvmv5p",
+#     apiKey = "da073fa6d424d4126e65a8a054a6693de19f485e",
+#     server = "http://127.0.0.1:8484/"
+#   )
+#   # var emails: OrderedTable[int, JsonNode]
+#   # for elem in waitFor grist.fetchTable("Emails"):
+#   #   echo elem
+#   #   emails[elem["id"].getInt] = elem
+#   #   # echo elem{"fields", "User"}.getStr(), " " ,  elem{"fields", "Email"}.getStr()
+#   #   # echo elem
 
-  var users: OrderedTable[int, JsonNode]
-  for elem in waitFor grist.fetchTable("Users", filters = {"FirstName": "Ivan"}.toTable()):
-    # echo elem
-    # users[elem["id"].getInt] = elem
-    echo elem{"fields", "FullName"}.getStr().alignLeft(40), " ", elem{"fields", "Email"}.getElems()
-    # echo elem{"fields", "User"}.getStr(), " " ,  elem{"fields", "Email"}.getStr()
-    # echo elem
+#   var users: OrderedTable[int, JsonNode]
+#   for elem in waitFor grist.fetchTable("Users", filters = {"FirstName": "Ivan"}.toTable()):
+#     # echo elem
+#     # users[elem["id"].getInt] = elem
+#     echo elem{"fields", "FullName"}.getStr().alignLeft(40), " ", elem{"fields", "Email"}.getElems()
+#     # echo elem{"fields", "User"}.getStr(), " " ,  elem{"fields", "Email"}.getStr()
+#     # echo elem
 
-  # for emailElem in emails.values:
-    # echo users[emailElem{"id"}.getInt]{"fields", "FullName"}
+#   # for emailElem in emails.values:
+#     # echo users[emailElem{"id"}.getInt]{"fields", "FullName"}
 
